a2cd2fc7b85f2c38d8a45f70876dcbd1dab30945
Shiva804 <krishnansai49@gmail.com>
2020-09-05 18:14:53 +0530

    Updates

---
+++ precache-manifest.2d1f44d761c0b514261d9a2ff9505cd7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f640cb65f614f2b277f92646363c9a7a",
+    "url": "/News-Today/index.html"
+  },
+  {
+    "revision": "483afba19d7e84c6bd48",
+    "url": "/News-Today/static/css/main.be40b5ee.chunk.css"
+  },
+  {
+    "revision": "9d8c06793250724494b9",
+    "url": "/News-Today/static/js/2.1c7bc166.chunk.js"
+  },
+  {
+    "revision": "70a4808e014a7625184f8c5ab94fa2d6",
+    "url": "/News-Today/static/js/2.1c7bc166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "483afba19d7e84c6bd48",
+    "url": "/News-Today/static/js/main.0718f029.chunk.js"
+  },
+  {
+    "revision": "6039b39427e27c2b0c5f",
+    "url": "/News-Today/static/js/runtime-main.de533347.js"
+  },
+  {
+    "revision": "d8d37e8089341e3eb71490f68a764812",
+    "url": "/News-Today/static/media/1020px-SARS-CoV-2_without_background.d8d37e80.png"
+  },
+  {
+    "revision": "6f15463b2490d44e8d7305845ef4cb77",
+    "url": "/News-Today/static/media/ScreenShot_20200712182605.6f15463b.png"
+  },
+  {
+    "revision": "1228105566fd9bd6a490ca44912d0559",
+    "url": "/News-Today/static/media/pexels-cottonbro-3944454-1.12281055.jpg"
+  }
+]);
